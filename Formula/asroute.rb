a72c8297513803814e086ce8e3104b718002b2ca
Steven Pack <steven.pack.code@gmail.com>
2020-09-02 21:54:58 -0700

    license

--- Formula/asroute.rb
+++ Formula/asroute.rb
@@ -3,6 +3,7 @@ class Asroute < Formula
   homepage "https://github.com/stevenpack/asroute"
   url "https://github.com/stevenpack/asroute/releases/download/v0.1.0/asroute-0.1.0-x86_64-apple-darwin.tar.gz"
   sha256 "a0b0988c5aef15908145061bc8e040b8bf5a60de6a5158dd587f5428453c3530"
+  license "MIT"
 
   def install
     bin.install "asroute"
